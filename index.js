372d2fd0e4c302ab4f8ad19d03c8363008e50163
Jose Valderez <valderezpe@gmail.com>
2021-11-20 12:48:21 -0300

    BodyParser adicionado e criação endpoints criação e atualização

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 const port = 3010
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Backend_API')
@@ -20,6 +23,24 @@ app.get('/mensagens/:id', (req, res) => {
 
   res.send(mensagem)
 })
+
+// [POST] / mensagens - Criar uma nova mensagem
+app.post('/mensagens', (req, res) => {
+  const mensagem = req.body.mensagem
+  mensagens.push(mensagem)
+
+  res.send(`Mensagem criada com sucesso: '${mensagem}'.`)
+})
+
+// [PUT] / mensagens {id} -- Atualiza uma mensagem pelo ID
+app.put('/mensagem/:id', (req, res) => {
+  const id = req.params.id - 1
+  const mensagem = req.body.mensagem
+  mensagens[id] = mensagem
+
+  res.send(`Mensagem atualizada com sucesso:'${mensagem}'.`)
+})
+
 app.listen(port, () => {
   console.info(`App rodando em http://localhost:${port}`)
 })
