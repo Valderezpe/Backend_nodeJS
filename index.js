d3c212b30aad5ba24c37ed748b6c2163060f3f11
Jose Valderez <valderezpe@gmail.com>
2021-11-20 14:38:02 -0300

    Modificação dos endpoints para trabalhar com listas de objetos

--- index.js
+++ index.js
@@ -10,7 +10,16 @@ app.get('/', (req, res) => {
   res.send('Backend_API')
 })
 
-const mensagens = ['Essa é a primeira mensagem', 'Essa é a segunda mensagem']
+const mensagens = [
+  {
+    id: 1,
+    texto: 'Essa é a primeira mensagem'
+  },
+  {
+    id: 2,
+    texto: 'Essa é a segunda mensagem'
+  }
+]
 
 app.get('/mensagens', (req, res) => {
   res.send(mensagens)
@@ -21,24 +30,44 @@ app.get('/mensagens/:id', (req, res) => {
   const id = req.params.id - 1
   const mensagem = mensagens[id]
 
+  if (!mensagem) {
+    res.send('Mensagem não encontrada.')
+
+    return
+  }
+
   res.send(mensagem)
 })
 
 // [POST] / mensagens - Criar uma nova mensagem
 app.post('/mensagens', (req, res) => {
-  const mensagem = req.body.mensagem
+  const mensagem = req.body
+
+  if (!mensagem || !mensagem.texto) {
+    res.send('Mensagem inválida.')
+
+    return
+  }
+  mensagem.id = mensagens.length + 1
   mensagens.push(mensagem)
 
-  res.send(`Mensagem criada com sucesso: '${mensagem}'.`)
+  res.send(mensagem)
 })
 
 // [PUT] / mensagens {id} -- Atualiza uma mensagem pelo ID
 app.put('/mensagens/:id', (req, res) => {
   const id = req.params.id - 1
-  const mensagem = req.body.mensagem
-  mensagens[id] = mensagem
+  const mensagem = mensagens[id]
+  const novoTexto = req.body.texto
 
-  res.send(`Mensagem atualizada com sucesso:'${mensagem}'.`)
+  if (!novoTexto) {
+    res.send('Mensagem inválida.')
+
+    return
+  }
+  mensagem.texto = novoTexto
+
+  res.send(mensagem)
 })
 
 // [DELETE] /mensagens {id}  Remover uma mensagem pelo ID
