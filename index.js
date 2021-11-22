606fecdec37c966d5606bbd6b4b4a32d673de3f4
Jose Valderez <valderezpe@gmail.com>
2021-11-21 22:49:52 -0300

    MongoDB driver adicionado e endpoint de leitura e mensagens imprementado

--- index.js
+++ index.js
@@ -1,100 +1,106 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()
+const mongodb = require('mongodb');
+(async () => {
+  const connectionString = 'mongodb://localhost:27017/server_banco'
 
-const port = 3010
+  console.info('Conectando ao banco mongoDB...')
 
-app.use(bodyParser.json())
-
-app.get('/', (req, res) => {
-  res.send('Backend_API')
-})
-
-const mensagens = [
-  {
-    id: 1,
-    texto: 'Essa é a primeira mensagem'
-  },
-  {
-    id: 2,
-    texto: 'Essa é a segunda mensagem'
-  }
-]
-
-const getMensagensValidas = () => mensagens.filter(Boolean)
-
-const getMensagemById = id => getMensagensValidas().find(msg => msg.id === id)
-
-app.get('/mensagens', (req, res) => {
-  res.send(getMensagensValidas())
-})
-
-// [GET] / mensagens/ {id} - Retorna uma única mensagem pelo ID
-app.get('/mensagens/:id', (req, res) => {
-  const id = +req.params.id
-
-  const mensagem = getMensagemById(id)
-
-  if (!mensagem) {
-    res.send('Mensagem não encontrada.')
-
-    return
+  const options = {
+    useUnifiedTopology: true
   }
 
-  res.send(mensagem)
-})
+  const client = await mongodb.MongoClient.connect(connectionString, options)
 
-// [POST] / mensagens - Criar uma nova mensagem
-app.post('/mensagens', (req, res) => {
-  const mensagem = req.body
+  const app = express()
 
-  if (!mensagem || !mensagem.texto) {
-    res.send('Mensagem inválida.')
+  const port = 3010
 
-    return
-  }
-  mensagem.id = mensagens.length + 1
-  mensagens.push(mensagem)
+  app.use(bodyParser.json())
 
-  res.send(mensagem)
-})
+  app.get('/', (req, res) => {
+    res.send('Backend_API')
+  })
 
-// [PUT] / mensagens {id} -- Atualiza uma mensagem pelo ID
-app.put('/mensagens/:id', (req, res) => {
-  const id = +req.params.id
+  const db = client.db('server_banco');
+  const mensagens = db.collection('mensagens');
+ 
+  const getMensagensValidas =() => mensagens.find({}).toArray();
 
-  const mensagem = getMensagemById(id)
+  const getMensagemById = id => getMensagensValidas().find(msg => msg.id === id)
 
-  const novoTexto = req.body.texto
+  app.get('/mensagens', async(req, res) => {
 
-  if (!novoTexto) {
-    res.send('Mensagem inválida.')
+    res.send(  await getMensagensValidas())
+  })
 
-    return
-  }
-  mensagem.texto = novoTexto
+  // [GET] / mensagens/ {id} - Retorna uma única mensagem pelo ID
+  app.get('/mensagens/:id', (req, res) => {
+    const id = +req.params.id
 
-  res.send(mensagem)
-})
+    const mensagem = getMensagemById(id)
 
-// [DELETE] /mensagens {id}  Remover uma mensagem pelo ID
-app.delete('/mensagens/:id', (req, res) => {
-  const id = +req.params.id
+    if (!mensagem) {
+      res.send('Mensagem não encontrada.')
 
-  const mensagem = getMensagemById(id)
+      return
+    }
 
-  if (!mensagem) {
-    res.send('Mensagem não encontrada.')
-    return
-  }
+    res.send(mensagem)
+  })
 
-  const index = mensagens.indexOf(mensagem)
+  // [POST] / mensagens - Criar uma nova mensagem
+  app.post('/mensagens', (req, res) => {
+    const mensagem = req.body
 
-  delete mensagens[index]
+    if (!mensagem || !mensagem.texto) {
+      res.send('Mensagem inválida.')
 
-  res.send('Mensagem removida com sucesso.')
-})
+      return
+    }
+    mensagem.id = mensagens.length + 1
+    mensagens.push(mensagem)
 
-app.listen(port, () => {
-  console.info(`App rodando em http://localhost:${port}`)
-})
+    res.send(mensagem)
+  })
+
+  // [PUT] / mensagens {id} -- Atualiza uma mensagem pelo ID
+  app.put('/mensagens/:id', (req, res) => {
+    const id = +req.params.id
+
+    const mensagem = getMensagemById(id)
+
+    const novoTexto = req.body.texto
+
+    if (!novoTexto) {
+      res.send('Mensagem inválida.')
+
+      return
+    }
+    mensagem.texto = novoTexto
+
+    res.send(mensagem)
+  })
+
+  // [DELETE] /mensagens {id}  Remover uma mensagem pelo ID
+  app.delete('/mensagens/:id', (req, res) => {
+    const id = +req.params.id
+
+    const mensagem = getMensagemById(id)
+
+    if (!mensagem) {
+      res.send('Mensagem não encontrada.')
+      return
+    }
+
+    const index = mensagens.indexOf(mensagem)
+
+    delete mensagens[index]
+
+    res.send('Mensagem removida com sucesso.')
+  })
+
+  app.listen(port, () => {
+    console.info(`App rodando em http://localhost:${port}`)
+  })
+})()
